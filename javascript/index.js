067f0852fd5377c5a4832b27fb8cdd2da59b2253
Oke <oakie.lukas@gmail.com>
2023-02-14 22:50:24 +0100

    okes first commit

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+const addInput = document.querySelector('addInput')
+const addItem = document.querySelector('#btnNewItem')
+const itemList = document.querySelector('.listHolder')
+
+addItem.addEventListener('click', () =>{
+    const ul = itemList.querySelector('ul');
+    const li =document.createElement('li');
+    ul.appendChild(li);
+});
+
+// Add an item
+
+// Create a new and empty item
+
+document.write('hello')
+
+
