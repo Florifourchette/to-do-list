efef2fa6e71bc38fcbd6da5fc4c698fa82e4527c
Florifourchette <florifourchette@gmail.com>
2023-02-15 18:07:09 +0100

    element can be stored and displayed

--- javascript/index.js
+++ javascript/index.js
@@ -3,9 +3,9 @@ const btnNewList = document.getElementById('btnNewlist');
 const storedList = Object.keys(localStorage);
 const displayListLocation = document.getElementById('overview-lists');
 
-btnNewList.addEventListener('click', () => {
-    localStorage.setItem("list name", listName.value);
-})
+btnNewList.addEventListener('click', () => 
+    localStorage.setItem(listName.value, listName.value)
+)
 
 storedList.forEach((element) => {
     const listName = localStorage.getItem(element);
@@ -13,4 +13,5 @@ const listContainer = document.createElement("div");
 listContainer.innerHTML = `List name = ${listName}`;
 displayListLocation.appendChild(listContainer);
 })
+
 
