60ed62a9aef7eddf1cbfd036c18c30994a805c17
Oke <oakie.lukas@gmail.com>
2023-02-16 21:19:27 +0100

    oke edit

--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,15 @@
-const listName = document.querySelector('input');
-const btnNewList = document.getElementById('btnNewlist');
+const listName = document.querySelector("input");
+const btnNewList = document.getElementById("btnNewlist");
 const storedList = Object.keys(localStorage);
-const displayListLocation = document.getElementById('overview-lists');
+const displayListLocation = document.getElementById("overview-lists");
 
-btnNewList.addEventListener('click', () => 
-    localStorage.setItem(listName.value, listName.value)
-)
+btnNewList.addEventListener("click", () =>
+  localStorage.setItem(listName.value, listName.value)
+);
 
 storedList.forEach((element) => {
-    const listName = localStorage.getItem(element);
-const listContainer = document.createElement("div");
-listContainer.innerHTML = `List name = ${listName}`;
-displayListLocation.appendChild(listContainer);
-})
-
-
+  const listName = localStorage.getItem(element);
+  const listContainer = document.createElement("li");
+  listContainer.innerHTML = `List name = ${listName}`;
+  displayListLocation.appendChild(listContainer);
+});
