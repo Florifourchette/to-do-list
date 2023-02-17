f2ba966dcf2893cf065882357efa95873515c93f
Oke <oakie.lukas@gmail.com>
2023-02-17 16:39:05 +0100

    CrossOutWorking

--- javascript/index.js
+++ javascript/index.js
@@ -2,6 +2,7 @@ const listName = document.querySelector("input");
 const btnNewList = document.getElementById("btnNewlist");
 const storedList = Object.keys(localStorage);
 const displayListLocation = document.getElementById("overview-lists");
+const myList = document.querySelector("#overview-lists");
 
 btnNewList.addEventListener("click", () =>
   localStorage.setItem(listName.value, listName.value)
@@ -10,103 +11,20 @@ btnNewList.addEventListener("click", () =>
 storedList.forEach((element) => {
   const listName = localStorage.getItem(element);
   const listContainer = document.createElement("li");
-  const listItem = listContainer.setAttribute('id', listName);
-  listContainer.innerHTML = `List name = ${listName}`;
+  /* const listItem = listContainer.setAttribute("id", listName); //added by Oke, attempt to target via id */
+  listContainer.innerHTML = `${listName}`;
   displayListLocation.appendChild(listContainer);
 });
 
+function crossOut(event) {
+  if (event.target.style.textDecoration === "line-through") {
+    event.target.style.textDecoration = "none";
+  } else {
+    event.target.style.textDecoration = "line-through";
+  }
+}
 
+myList.addEventListener("click", crossOut);
+/* myList.removeEventListener("click", crossOut); */
 
-
-
-
-/* const crossOut = () => {
-  document
-    .getElementById(className)
-    .style.setProperty("text-decoration", "line-through");
-};
-
-const listElements = document.querySelectorAll(".crossOut");
-listElements.forEach((element) => {
-  element.addEventListener("click", (event) => {
-    const id = event.target.id;
-    crossOut(id);
-  });
-}); */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* const listName = document.querySelector("input");
-const btnNewList = document.getElementById("btnNewlist");
-const storedList = Object.keys(localStorage);
-const displayListLocation = document.getElementById("overview-lists");
-
-//to target the individual list items
-/* let listItems = document.querySelector(".itemDone"); */
-
-/* btnNewList.addEventListener("click", () =>
-  localStorage.setItem(listName.value, listName.value)
-);
-
-const crossOut = (className) => {
-  document
-    .getElementsByClassName(className)
-    .style.setProperty("text-decoration", "line-through");
-};
-
-const listElements = document.querySelectorAll(".crossOut");
-listElements.forEach((element) => {
-  element.addEventListener("click", (event) => {
-    const id = event.target.id;
-    crossOut(id);
-  });
-});
-
-storedList.forEach((element) => {
-  const nameList = localStorage.getItem(element);
-  const listContainer = document.createElement("li");
-  listContainer.innerHTML = `List name = ${nameList}`;
-  displayListLocation.appendChild(listContainer);
-
-  // add class to new elements
-  document.querySelector(li).classList.add("checked"); //added by Oke
-}); */
-
-// cross out list item: target class
-
-// uncross list item
-
-/* if (crossOut) {
-displayListLocation.removeEventListener('click', crossOut);
-} */
-
-/* storedList.forEach((key) => {
-  const localValue = localStorage.getItem(key);
-  const localValueContainer = document.createElement("div");
-  localValueContainer.innerHTML = `List name = ${key}`;
-  localValueContainer.className = "crossOut";
-  localValueContainer.id = key;
-
-  displayListLocation.appendChild(localValueContainer); */
-/* document
-    .getElementsByClassName(key)
-    .addEventListener("click", (event) => crossOut(key)); 
-// add class to new elements
-//});
- */
+/* function  */
